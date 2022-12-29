d161e5443274d891c07506873dcc38b1d4b3349e
Elin A <elina@Elins-MacBook-Air.local>
2022-12-29 01:33:13 +0100

    halvvägs klar

---
+++ app/src/main/java/com/example/au22_flashcard/WordDao.kt
@@ -0,0 +1,27 @@
+package com.example.au22_flashcard
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
+interface WordDao {
+
+    @Insert
+    fun insert(word: Word)
+
+    @Delete
+    fun delete(delete: Delete)
+
+    @Query("SELECT * FROM word_table")
+    fun getAllWords() : List<Word>
+
+
+}
+
+// @Query("SELECT * FROM item_table")
+//    fun getAll() : List<Item>
+//
+//    @Query("SELECT * FROM item_table WHERE category LIKE :categoryName")
+//    fun findByCategory(categoryName: String) : List<Item>
